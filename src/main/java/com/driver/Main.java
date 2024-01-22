25291f996c620615f4e11895774b8ae68aad5a51
Likhitha <veshalalikhitha@gmail.com>
2024-01-22 18:33:20 +0530

    Polymorphism

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,20 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String args[]){
+        Product p = new Product();
+        int result = p.product(10,20,30);
+        double dbans = p.product(3.2,4.5);
+    }
+    static class Product{
+        public int product(int x,int y){
+            return x*y;
+        }
+        public int product(int x,int y,int z){
+            return x*y*z;
+        }
+        public double product(double x,double y){
+            return x*y;
+        }
+    }
 }
